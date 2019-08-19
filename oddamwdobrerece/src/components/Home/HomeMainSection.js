04bcd296d26546eaae42c702f3270bcd1d7ddb90
lukaszwachowiak <lukasz.wachowiak@op.pl>
2019-08-19 22:17:33 +0200

    home/aboutUs

--- oddamwdobrerece/src/components/Home/HomeMainSection.js
+++ oddamwdobrerece/src/components/Home/HomeMainSection.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+import HomeHeroImage from "../../assets/Home-Hero-Image.jpg";
 
 class HomeMainSection extends Component {
     render(){
         return (
             <article className="mainSectionContainer">
                 <section className="mainSectionLeft">
-                    {/*<img />*/}
+                    <img src={HomeHeroImage} alt="HomeHeroImage" className="mainSectionImage"/>
                     {/*https://create-react-app.dev/docs/adding-images-fonts-and-files*/}
                 </section>
                 <section className="mainSectionRight">
@@ -14,7 +16,7 @@ class HomeMainSection extends Component {
                         <p>Oddaj niechciane rzeczy w zaufane ręce</p>
                     </div>
                     <div className="subSectionLower">
-                        <p>ODDAJ <br/> RZECZY</p>
+                        <Link to="/logowanie/"><p>ODDAJ <br/> RZECZY</p></Link>
                         <p>ZORGANIZUJ <br/> ZBIÓRKĘ</p>
                     </div>
                 </section>
