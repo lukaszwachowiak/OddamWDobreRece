913ccb0044115f9bf91bc71c603d3da9b596f58b
lukaszwachowiak <lukasz.wachowiak@op.pl>
2019-08-22 16:33:30 +0200

    footer

--- oddamwdobrerece/src/components/Home/HomeContact.js
+++ oddamwdobrerece/src/components/Home/HomeContact.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
+import clothes from "../../assets/Background-Contact-Form.jpg";
 
 class HomeContact extends Component {
     render(){
         return (
-            <div id="contact">contact</div>
+            <div id="contact" className="contactContainer">
+                <div className="contactImage">
+                    {/*<img src={clothes} alt="clothes"/>*/}
+                </div>
+                <div className="contactSectionRight">
+                    <h2 className="contactSectionRightHeader">Skontaktuj się z nami</h2>
+                    <Decoration/>
+                    <form className="">
+                        <div>
+                            <div className="">
+                                <label htmlFor="name">Wpisz swoje imię </label>
+                                <input type="text" name="name" placeholder="Krzysztof" required/>
+                            </div>
+                            <div className="">
+                                <label htmlFor="email">Wpisz swój e-mail </label>
+                                <input type="email" name="email" placeholder="abc@xyz.pl" required/>
+                            </div>
+                        </div>
+                        <div>
+                            <textarea></textarea>
+                        </div>
+                        <div className="form-example">
+                            <input type="submit" value="Wyślij"/>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
         )
     }
 }
--- oddamwdobrerece/src/components/HomeFooter.js
+++ oddamwdobrerece/src/components/HomeFooter.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import facebookIcon from "../assets/Facebook.svg";
+import instagramIcon from "../assets/Instagram.svg";
 
 class HomeFooter extends Component {
     render(){
         return (
-            <footer>footer</footer>
+            <footer className="footerContainer">
+                <div className="footerBlankSpace"></div>
+                <h3 className="footerHeader">Copyright by Coders Lab</h3>
+                <div className="footerSocialMedia">
+                    <img src={facebookIcon} alt="facebook icon" className="footerIcon"/>
+                    <img src={instagramIcon} alt="instagram icon" className="footerIcon"/>
+                </div>
+            </footer>
         )
     }
 }
