2f54e586ac7182359a1004fcd86dbf7469b86477
lukaszwachowiak <lukasz.wachowiak@op.pl>
2019-08-27 17:11:21 +0200

    2a logowanie - layout

--- oddamwdobrerece/src/components/Log.js
+++ oddamwdobrerece/src/components/Log.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Decoration} from "../assets/Decoration.svg";
 
 const Log = () => {
     return (
-        <>logowanie</>
+        <div className="logContainer">
+            <h2 className="logHeader">Zaloguj się</h2>
+            <Decoration className="logDecoration"/>
+            <form className="logFormContainer">
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder=""
+                    // value={this.state.name.value}
+                    // onChange={}
+                />
+                <label htmlFor="password">Hasło</label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder=""
+                    // value={this.state.name.value}
+                    // onChange={}
+                />
+            </form>
+            <div className="logSmallContainer">
+                <ul>
+                    <li>
+                        <Link to="/rejestracja/">Załóż konto</Link>
+                    </li>
+                </ul>
+
+                <input type="submit" value="Zaloguj się"/>
+            </div>
+        </div>
     )
 };
 
